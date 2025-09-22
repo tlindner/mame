4880504927b6651035dd41e1ac1cbcbc4c3f66ba
tim lindner <tlindner@macmess.org>
2025-09-21 18:01:46 -0700

    cleanup

--- src/devices/machine/6883sam.h
+++ src/devices/machine/6883sam.h
@@ -42,23 +42,6 @@ public:
 
 protected:
 	// SAM state constants
-	static constexpr uint16_t SAM_STATE_TY = 0x8000;
-	static constexpr uint16_t SAM_STATE_M1 = 0x4000;
-	static constexpr uint16_t SAM_STATE_M0 = 0x2000;
-	static constexpr uint16_t SAM_STATE_R1 = 0x1000;
-	static constexpr uint16_t SAM_STATE_R0 = 0x0800;
-	static constexpr uint16_t SAM_STATE_P1 = 0x0400;
-	static constexpr uint16_t SAM_STATE_F6 = 0x0200;
-	static constexpr uint16_t SAM_STATE_F5 = 0x0100;
-	static constexpr uint16_t SAM_STATE_F4 = 0x0080;
-	static constexpr uint16_t SAM_STATE_F3 = 0x0040;
-	static constexpr uint16_t SAM_STATE_F2 = 0x0020;
-	static constexpr uint16_t SAM_STATE_F1 = 0x0010;
-	static constexpr uint16_t SAM_STATE_F0 = 0x0008;
-	static constexpr uint16_t SAM_STATE_V2 = 0x0004;
-	static constexpr uint16_t SAM_STATE_V1 = 0x0002;
-	static constexpr uint16_t SAM_STATE_V0 = 0x0001;
-
 	static constexpr int SAM_BIT_V0 = 0;
 	static constexpr int SAM_BIT_V1 = 1;
 	static constexpr int SAM_BIT_V2 = 2;
@@ -75,6 +58,23 @@ protected:
 	static constexpr int SAM_BIT_M0 = 13;
 	static constexpr int SAM_BIT_M1 = 14;
 	static constexpr int SAM_BIT_TY = 15;
+
+	static constexpr uint16_t SAM_STATE_V0 = 1U << SAM_BIT_V0;
+	static constexpr uint16_t SAM_STATE_V1 = 1U << SAM_BIT_V1;
+	static constexpr uint16_t SAM_STATE_V2 = 1U << SAM_BIT_V2;
+	static constexpr uint16_t SAM_STATE_F0 = 1U << SAM_BIT_F0;
+	static constexpr uint16_t SAM_STATE_F1 = 1U << SAM_BIT_F1;
+	static constexpr uint16_t SAM_STATE_F2 = 1U << SAM_BIT_F2;
+	static constexpr uint16_t SAM_STATE_F3 = 1U << SAM_BIT_F3;
+	static constexpr uint16_t SAM_STATE_F4 = 1U << SAM_BIT_F4;
+	static constexpr uint16_t SAM_STATE_F5 = 1U << SAM_BIT_F5;
+	static constexpr uint16_t SAM_STATE_F6 = 1U << SAM_BIT_F6;
+	static constexpr uint16_t SAM_STATE_P1 = 1U << SAM_BIT_P1;
+	static constexpr uint16_t SAM_STATE_R0 = 1U << SAM_BIT_R0;
+	static constexpr uint16_t SAM_STATE_R1 = 1U << SAM_BIT_R1;
+	static constexpr uint16_t SAM_STATE_M0 = 1U << SAM_BIT_M0;
+	static constexpr uint16_t SAM_STATE_M1 = 1U << SAM_BIT_M1;
+	static constexpr uint16_t SAM_STATE_TY = 1U << SAM_BIT_TY;
 
 	// incidentals
 	required_device<cpu_device> m_cpu;
